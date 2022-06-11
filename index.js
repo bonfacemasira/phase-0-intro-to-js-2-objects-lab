80005898248c59680a5803d780bcd62239ea7e22
Bonface Masira <bonfacemasira@gmail.com>
2022-06-11 23:20:21 +0300

    commit tree 0ecd293ed613d8207576c9f826c95c34012b7db7

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+    name:'Bonface',
+    streetAddress: 'Kahawa',
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]:value,
+    };
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    'name',
+    'Meshack'
+)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+    return obj;
+}
+
+const newStreetAddress = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', 'Thika Road');
+
+function deleteFromEmployeeByKey(employee, key){
+    const veryNewEmployee = {...employee};
+
+    delete veryNewEmployee.name;
+    delete veryNewEmployee.streetAddress;
+
+    return veryNewEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    delete employee.streetAddress;
+
+    return employee;
+}
